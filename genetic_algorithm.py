a966c37bb7c161c2fc0d2cebd212c516213fbaa6
omsf <ocimar.acad@gmail.com>
2019-11-08 10:26:56 -0300

    Population is returned ordered by MSE; MSE used as stopping criterion

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -5,14 +5,13 @@ from data_analyzer import *
 
 class GeneticAlgorithm:
     population = list()
-    fitness = list()
+    MSE = list()
 
-    def __init__(self, population_size = 1000, chromosome_size = 10, gene_max = 255, num_loops = 100):
+    def __init__(self, population_size=1000, chromosome_size=10, gene_max=255):
         # Constantes:
-        self.population_size = 2*(population_size//2) # must be even
+        self.population_size = 2*(population_size//2)  # must be even
         self.chromosome_size = chromosome_size
         self.gene_max = gene_max
-        self.num_loops = num_loops
 
     def createPopulation(self):
         # Cria a polulação:
@@ -21,11 +20,12 @@ class GeneticAlgorithm:
 
     def proportionateSelection(self):
         probabilities = list()
-        fitness_sum = np.sum(self.fitness)
+        fitness = np.divide(1, self.MSE)
+        fitness_sum = np.sum(fitness)
         previous_probability = 0.0
 
         for i in range(0, self.population_size - 1):
-            probabilities.append(previous_probability + (self.fitness[i] / fitness_sum))
+            probabilities.append(previous_probability + (fitness[i] / fitness_sum))
 
         rand = random.random()
         for subject in range(0, self.population_size-1):
@@ -38,7 +38,7 @@ class GeneticAlgorithm:
         sis = parents[1][0:rand] + parents[0][rand::]
         return [bros, sis]
 
-    def mutation(self, son, mutation_rate = 0.1):
+    def mutation(self, son, mutation_rate=0.1):
         for gene in range(0, self.chromosome_size-1):
             if random.random() < mutation_rate:
                 son[gene] = random.randint(0, self.gene_max)
@@ -47,32 +47,40 @@ class GeneticAlgorithm:
                 son[gene] = random.randint(0, self.gene_max)
         return son
 
-    def evolve(self, grammar, crossing_probability=0.8, mutation_rate=0.1):
-        # Loop de evolução
-        for generation in range(1, self.num_loops):
+    def evolve(self, grammar, filename, crossing_probability=0.8, mutation_rate=0.1, num_loops=1000,
+               satisfactory_MSE=0.01):
+        # Assessment:
+        MSEcalculator = DataAnalyzer(filename)
+        for chromosome in self.population:
+            expr = grammar.derivateExpression(chromosome)
+            self.MSE.append(MSEcalculator.mean_squared_error(expr))
+        max_MSE = max(self.MSE)
 
-            # Assessment:
-            MSEcalculator = DataAnalyzer('training.csv')
-            for chromosome in self.population:
-                expr = grammar.derivateExpression(chromosome)
-                self.fitness.append(1.0/MSEcalculator.mean_squared_error(expr))
-
+        # Evolve loop
+        generation = 0
+        while generation < num_loops and max_MSE > satisfactory_MSE:
             # Selection, crossing and mutation:
             for parents_index in range(0, self.population_size//2-1):
                 # Selection:
                 parents = [self.proportionateSelection(), self.proportionateSelection()]
                 rand = random.random()
-                # Crossing:
                 if rand < crossing_probability:
+                    # Crossing:
                     children = self.crossover(parents)
-                    # Mutation:
-                    self.population[parents_index] = self.mutation(children[0], mutation_rate)
-                    self.population[parents_index+1] = self.mutation(children[1], mutation_rate)
                 else:
-                    # if it don't cross, just mutate:
-                    self.population[parents_index] = self.mutation(parents[0], mutation_rate)
-                    self.population[parents_index + 1] = self.mutation(parents[1], mutation_rate)
+                    # if it don't cross, just pass:
+                    children = parents
+                # Mutation:
+                self.population[parents_index] = self.mutation(children[0], mutation_rate)
+                self.population[parents_index+1] = self.mutation(children[1], mutation_rate)
 
-        return self.population # [x for _, x in sorted(zip(fitness,self.population))]
+            generation = generation+1
+            # Assessment:
+            # (Here due to the stopping criterion)
+            for chromosome in self.population:
+                expr = grammar.derivateExpression(chromosome)
+                self.MSE.append(MSEcalculator.mean_squared_error(expr))
+            max_MSE = max(self.MSE)
 
-
+        # Population is returned ordered by MSE
+        return [x for _, x in sorted(zip(self.MSE, self.population))]
